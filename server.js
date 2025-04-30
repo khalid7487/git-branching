f97d17e1ec8c66b75d6ef3a1db49c3c43f668ccf
khalid7487 <khalid15-7487@gmail.com>
2025-04-30 20:55:24 +0600

    modified ceo namem

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello js");
+});
+
+app.listen(port, () => {
+  `Server started on port ${port}`;
+});
